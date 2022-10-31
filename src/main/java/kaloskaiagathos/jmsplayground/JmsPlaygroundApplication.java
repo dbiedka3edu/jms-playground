acfa19b6555bd0ded8eb727fb94b3c1437d0ded8
danbie <danbie@st.amu.edu.pl>
2022-10-31 12:22:11 +0100

    initial commit

---
+++ src/main/java/kaloskaiagathos/jmsplayground/JmsPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package kaloskaiagathos.jmsplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmsPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kaloskaiagathos/jmsplayground/JmsPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package kaloskaiagathos.jmsplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
